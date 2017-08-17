363c0ad6423c06e5938cf53c9eb88f00e00ff950
Iggy <igoririanto@rocketmail.com>
2017-08-16 19:02:18 -0700

    add filter wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,75 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import {observable, action} from 'mobx';
 import DevTools from 'mobx-react-devtools';
+//import './App.css';
+
+const styles = {
+  listStyle: {
+    listStyleType: 'none'
+  }
+}
 
 @observer
 class TodoList extends Component {
   render(){
     const todo = this.props.todo;
+    const store = this.props.store;
+    const index = this.props.index;
     return (
-      <li>{todo} <a href="#" onClick={() => this.onTodoClick(this.props.index)}>X</a></li>
-    )
-  };
-
-  @action onTodoClick = (index) => {
-    this.props.removeTodo(index);
-  };
+      <li><input type="checkbox" checked={todo.completed} onChange={() => store.handleToggle(index)}/>
+        {todo.task} <a href="#" onClick={() => store.removeTodo(index)}>X</a>
+      </li>
+    )};
 }
 
 @observer
 class App extends Component {
-
   render() {
     const todos = this.props.store.todos;
     return (
       <div>
         <h1>React + MobX Todo Demo</h1>
-        <h4>You have: {this.props.store.todoLength} Todos</h4>
-        <ul>
+        <h4>You have {this.props.store.todoLength} Todos</h4>
+        <h4>You have {this.props.store.completedTodoLength} completed Todos</h4>
+
+        <ul style={styles.listStyle}>
           {todos.map((todo, index) => {
-            return <TodoList todo={todo} index={index} key={index} removeTodo={this.removeTodo} />
+            return <TodoList todo={todo} index={index} key={todo.id} store={this.props.store} />
             }
           )}
         </ul>
+
         <input value={this.input} onChange={this.handleNewTodo} /><button onClick={this.addTodo}>Add</button>
+        <div>
+          <button onClick={() => this.props.store.setFilterToCompleted()}>Show Completed</button>
+          <button>Delete All Completed</button>
+        </div>
         <DevTools />
       </div>
     );
   };
-
   @observable input = '';
-
   @action addTodo = () => {
     this.props.store.addTodo(this.input);
     this.input = '';
   };
-
   @action handleNewTodo = (e) => {
     this.input = e.target.value;
   };
-
-  @action removeTodo = (index) => {
-    this.props.store.removeTodo(index);
+  @action handleFilter = () => {
+    this.props.store.setFilterToCompleted('completed');
   };
+
+  @action getCompletedTodos = () => {
+    return this.props.store.todos.filter((todo) => {
+      switch (this.props.store.todoFilter) {
+        case 'completed':
+          return todo.completed;
+        default:
+          return true;
+      }
+    })
+  }
 };
 
 export default App;
--- src/TodoStore.js
+++ src/TodoStore.js
@@ -1,20 +1,58 @@
 import {observable, action, computed, useStrict} from 'mobx';
+import * as Utils from './utils.js';
 
 useStrict(true);
 
 class TodoStore {
-  @observable todos = ['learn mobx', 'make mobx app', 'be mobx jedi'];
+  @observable todos = [{task: 'learn mobx', completed: false, id: Utils.guid()}, {task: 'make mobx app', completed: false, id: Utils.guid()}, {task: 'be mobx jedi', completed: false, id: Utils.guid()}];
+  @observable todoFilter = 'all';
 
+  constructor(){
+    this.todos.map((todo) => {
+      console.log("task: " + todo.task);
+      console.log("id: " + todo.id);
+      console.log("completed: " + todo.completed);
+    })
+  }
+  @computed get completedTodoLength(){
+    return this.todos.filter((todo) => {
+      return todo.completed == true
+    }).length;
+  };
   @computed get todoLength(){
     return this.todos.length;
   };
-
   @action addTodo(todoItem) {
-    this.todos = [todoItem, ...this.todos]
+    this.todos.push({task: todoItem, completed: false, id: Utils.guid()})
+    //this.todos = [{task: todoItem, completed: false, id: Utils.guid()}, ...this.todos]
   };
   @action removeTodo(index){
     this.todos.splice(index, 1);
+    console.log(this.todos);
   };
+  @action handleToggle(index){
+    this.todos[index].completed = !this.todos[index].completed;
+    console.log(this.todos[index].completed);
+  };
+  @action setFilterToCompleted(){
+    //this.todoFilter = newFilter;
+    //console.log(this.todoFilter + "before");
+    this.todoFilter = 'completed';
+    console.log(this.todoFilter + " after");
+  };
+
+  @action filterCompletedTodos(){
+    return this.todos.filter((todo) => {
+      switch (this.todoFilter){
+          case 'completed':
+            return todo.completed;
+          case 'active':
+            return !todo.completed;
+          default:
+            return true;
+          }
+    })
+  }
 }
 
 export default TodoStore;
